196373ff8692f574ae8fb4bfc96962a0980e1399
Anthony Cotrina <anthony77CG@hotmail.com>
2025-07-14 20:30:29 -0500

    First commit

--- src/app/app.js
+++ src/app/app.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hola desde la aplicación contenedor!');
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Aplicación contenedor escuchando en http://localhost:${port}`);
 });
